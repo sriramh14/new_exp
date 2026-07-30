4a1ad26dffdbb9d688b02f7dd16b1ccc50293eed
sriramh14 <sriramhari1014@gmail.com>
2026-07-31 02:36:30 +0530

    Update dataset_loader.py

--- dataset/dataset_loader.py
+++ dataset/dataset_loader.py
@@ -1,4 +1,4 @@
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset,DataLoader
 import numpy as np
 import random
 import cv2
@@ -7,9 +7,10 @@ import h5py
 class TrainDataset(Dataset):
     def __init__(self, dir_rgb, dir_hsi, train_list_dir, bgr2rgb=True, train = True):
 
-        #List to store hyper and rgb imgs
-        self.hypers = []
-        self.bgrs = []
+        #List to store hyper and rgb img paths 
+        #Not storing images as it consumes too much space
+        self.hyper_list = []
+        self.bgr_list = []
         h,w = 482,512  # img shape
         if train:
             hyper_data_path = f'{dir_hsi}/Train_spectral/'
@@ -20,49 +21,52 @@ class TrainDataset(Dataset):
 
         #Opens list file and saves the list as array
         with open(train_list_dir, 'r') as fin:
-            hyper_list = [line.replace('\n','.mat') for line in fin]
-            bgr_list = [line.replace('mat','jpg') for line in hyper_list]
-        hyper_list.sort()
-        bgr_list.sort()
-        print(f'len(hyper) of ntire2022 dataset:{len(hyper_list)}')
-        print(f'len(bgr) of ntire2022 dataset:{len(bgr_list)}')
-        for i in range(len(hyper_list)):
-            hyper_path = hyper_data_path + hyper_list[i]
-            if 'mat' not in hyper_path:
+            self.hyper_list = [line.replace('\n','.mat') for line in fin]
+            self.bgr_list = [line.replace('mat','jpg') for line in self.hyper_list]
+        self.hyper_list.sort()
+        self.bgr_list.sort()
+        print(f'len(hyper) of ntire2022 dataset:{len(self.hyper_list)}')
+        print(f'len(bgr) of ntire2022 dataset:{len(self.bgr_list)}')
+        for i in range(len(self.hyper_list)):
+
+            #check to see if the file names are same
+            assert self.hyper_list[i].split('.')[0] ==self.bgr_list[i].split('.')[0], 'Hyper and RGB come from different scenes.'
+
+            #Check to see if mat is present in the file name
+            if 'mat' not in self.hyper_list[i]:
                 continue
 
-            #Reading hsi file
-            with h5py.File(hyper_path, 'r') as mat:
-                hyper =np.float32(np.array(mat['cube']))
-            hyper = np.transpose(hyper, [0, 2, 1])
-
+            #Creating array containing path of the files
+            self.hyper_list[i] = hyper_data_path + self.hyper_list[i]
+            self.bgr_list[i] = bgr_data_path + self.bgr_list[i]
             
-            bgr_path = bgr_data_path + bgr_list[i]
-
-            #check to see if rgb and hsi are from same scene
-            assert hyper_list[i].split('.')[0] ==bgr_list[i].split('.')[0], 'Hyper and RGB come from different scenes.'
-
-            #Reading rgb file and then converting from bgr to rgb
-            bgr = cv2.imread(bgr_path)
-            if bgr2rgb:
-                bgr = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
-            bgr = np.float32(bgr)
-
-            #Normalisation? Im not too sure 
-            bgr = (bgr-bgr.min())/(bgr.max()-bgr.min())
-            bgr = np.transpose(bgr, [2, 0, 1])  # [3,482,512]
-            self.hypers.append(hyper)
-            self.bgrs.append(bgr)
-            mat.close()
-            print(f'Ntire2022 scene {i} is loaded.')
-        self.img_num = len(self.hypers)
+            
+        self.img_num = len(self.hyper_list)
         self.length = self.img_num
 
     
     def __getitem__(self, idx):
-        bgr = self.bgrs[idx]
-        hyper = self.hypers[idx]
+
+        #Reading hsi file
+        with h5py.File(self.hyper_list, 'r') as mat:
+            hyper =np.float32(np.array(mat['cube']))
+            hyper = np.transpose(hyper, [0, 2, 1])
+
+        #Reading rgb file
+        bgr = cv2.imread(self.bgr_list)
+        #Converting to rgb from bgr 
+        if bgr2rgb:
+            bgr = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+            bgr = np.float32(bgr)
+            bgr = (bgr-bgr.min())/(bgr.max()-bgr.min())
+            bgr = np.transpose(bgr, [2, 0, 1])  # [3,482,512]
+            mat.close()
+
+        
         return np.ascontiguousarray(bgr), np.ascontiguousarray(hyper)
+
+
+    
 
     def __len__(self):
         return self.img_num
@@ -72,7 +76,8 @@ class TrainDataset(Dataset):
 if __name__ == "__main__":
     dir_rgb = "/kaggle/input/datasets/sriramhari14/ntire-2022/Train_RGB"
     dir_hsi = "/kaggle/input/datasets/sriramhari14/ntire-2022/Train_spectral"
-    dataset = TrainDataset(dir_rgb,dir_hsi)
+    train_list_dir = "/kaggle/input/datasets/sriramhari14/mst-train-list/train_list.txt"
+    dataset = TrainDataset(dir_rgb,dir_hsi,train_list_dir)
     print(TrainDataset.__len__(dataset))
     loader = DataLoader(
         dataset,
